55ddbaab76dce8c971da664b81591801a81e6267
Ratheesh <ratheesh@Ratheeshs-MacBook-Pro.local>
2024-10-23 18:29:53 +0100

    session1: basic topics

---
+++ programes/session1.py
@@ -0,0 +1,31 @@
+x = "hello"
+print(x)
+type(x)
+print(type(x))
+
+y = 9
+print(y)
+type(y)
+print(type(y))
+
+c = 9.0
+print(c)
+type(c)
+print(type(c))
+
+z = 3 + 5j
+print(z)
+print(type(z))
+
+fruits = ["apple" , "bananna" , "mango"]
+print(fruits)
+print(type(fruits))
+
+fruits[1] = "pappaya"
+print(fruits)
+
+name = "Aagney" + " " + "Ratheesh"
+print(name)
+
+name = "Aagney" * 3
+print(name)
