5caa613a4cce79e1881d04b501f6b6a9638baa53
Ratheesh <ratheesh@Ratheeshs-MacBook-Pro.local>
2024-11-15 17:42:04 +0000

    session10_1

--- programes/session10_1.py
+++ programes/session10_1.py
@@ -4,13 +4,13 @@
 # 7 - 1,7
 # 11- 1,11
 # 8 - 1,2,4,8
+# 12- 1,2,3,4,6,12
 # 21 - 1,3,7,21
 # num = 13
 # num % range(2,num) == 0
-
 num = int(input())
 flag = False
-for item in range(2,num):
+for item in range(2,int(num/2)+1):
     if num % item == 0:
         flag = True
         break
@@ -19,3 +19,4 @@ if flag == True:
     print("Not Prime")
 else:
     print("Prime")
+
