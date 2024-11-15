3d30e37ad9868a8ad3a332e186abe386e2850c6d
Ratheesh <ratheesh@Ratheeshs-MacBook-Pro.local>
2024-11-15 17:04:19 +0000

    session10

--- programes/session10.py
+++ programes/session10.py
@@ -5,14 +5,13 @@
 # 3 * 5 = 15
 # number * range(1,11) = result
 class MultiplicationTable:
-    def __init__(self):
-        self.number = int(input())
-
+    def __init__(self,number):
+        self.number = number
     def multiplication(self):
         for item in range(1, 11):
             print(f"{self.number} x {item} = {self.number * item}")
 
 
-
-multiplicant = MultiplicationTable()
+number = int(input())
+multiplicant = MultiplicationTable(number)
 multiplicant.multiplication()
