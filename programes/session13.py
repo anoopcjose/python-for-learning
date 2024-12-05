b282f397b465383fb1dd3a2099163db21c6b14ff
Ratheesh <ratheesh@Ratheeshs-MacBook-Pro.local>
2024-12-05 17:44:59 +0000

    session13

---
+++ programes/session13.py
@@ -0,0 +1,6 @@
+fruits = ['Apple','Banana','Mango','Kiwi','Strawberry']
+[print(fruit,' has n') for fruit in fruits if 'n' in fruit]
+
+# for fruit in fruits:
+#     if 'n' in fruit:
+#         print(fruit,'Fruit has n')
