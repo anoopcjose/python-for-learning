d498a90dcebbd62c1496bf05dfe6d46b2636a39a
Ratheesh <ratheesh@Ratheeshs-MacBook-Pro.local>
2024-12-10 18:06:04 +0000

    dictonary methods

---
+++ programes/dec_session2_3.py
@@ -0,0 +1,12 @@
+person= dict(name = "Aagney",age = 14 , country = "uk" )
+print(person)
+details = person.get("name")
+print(details)
+x= person.keys()
+print(x)
+person["subject"] = "maths"
+print(person)
+person["hobby"] = "Youtube"
+print(person)
+y = person.values()
+print(y)
---
+++ programes/dec_session2_4.py
@@ -0,0 +1,13 @@
+thiscar = {
+    "brand": "ford",
+    "year" : 1964,
+    "colour": ['red', 'white','black','yellow'],
+    "Electric": False
+}
+print(thiscar)
+thiscar["year"] = 2024
+print(thiscar)
+thiscar["varient"] = "blue"
+print(thiscar)
+z = thiscar.items()
+print(z)
