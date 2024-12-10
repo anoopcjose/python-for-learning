0e89138563192d76710fe31162b6800fd18397b1
Ratheesh <ratheesh@Ratheeshs-MacBook-Pro.local>
2024-12-10 17:35:42 +0000

    basics of dictionary

---
+++ programes/december_session2.py
@@ -0,0 +1,16 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+thisdict2 = {
+  "subject": "english",
+  "food": "pizza",
+  "game": "football"
+}
+print(thisdict2)
+print(thisdict2["subject"])
+print(thisdict2["game"])
+print(len(thisdict2))
